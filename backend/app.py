99bc08cea7d4a85e54f53d725991dcd29a7ca50f
MadPapa <happyekkov@gmail.com>
2023-02-05 22:53:42 +0100

    creating app with first model

---
+++ backend/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/flask'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class Articles(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    body = db.Column(db.Text)
+    date = db.Column(db.DateTime, default=datetime.datetime.now)
+
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/get', methods=['GET'])
+def get_articles():
+    return jsonify({"hello": "world"})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
