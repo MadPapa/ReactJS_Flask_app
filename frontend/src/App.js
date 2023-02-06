40ef79738535f4a025adc79b6efcfcebdaef69a2
MadPapa <happyekkov@gmail.com>
2023-02-06 18:01:55 +0100

    add editArticle function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import ArticleList from './components/ArticleList';
 function App() {
 
   const [articles, setArticles] = useState([])
+  const [editedArticle, setEditedArticle] = useState([])
 
   useEffect(() => {
     fetch('http://127.0.0.1:5000/get', {
@@ -19,11 +20,15 @@ function App() {
 
   }, [])
 
+  const editArticle = (article) => {
+    setEditedArticle(article)
+  }
+
   return (
     <div className="App">
       <h1>Flask and React App</h1>
 
-      <ArticleList articles={articles} />
+      <ArticleList articles={articles} editArticle={editArticle} />
 
     </div>
   );
