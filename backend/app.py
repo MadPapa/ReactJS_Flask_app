8cd672ae5246211739f2f2fbef19be0a1b7e3b77
MadPapa <happyekkov@gmail.com>
2023-02-06 12:24:00 +0100

    adding serializers

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 import datetime
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 
@@ -8,22 +9,51 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/flask'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 
+# classes
 class Articles(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     body = db.Column(db.Text)
     date = db.Column(db.DateTime, default=datetime.datetime.now)
 
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
 
+
+# serialization object
+class ArticleSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'title', 'body', 'date')
+
+
+article_schema = ArticleSchema()
+articles_schema = ArticleSchema(many=True)
+
+
+# creating database command
 with app.app_context():
     db.create_all()
 
 
+# app views
 @app.route('/get', methods=['GET'])
 def get_articles():
     return jsonify({"hello": "world"})
+
+
+@app.route('/add', methods=['POST'])
+def add_article():
+    title = request.json['title']
+    body = request.json['body']
+
+    articles = Articles(title, body)
+    db.session.add(articles)
+    db.session.commit()
+    return article_schema.jsonify(articles)
 
 
 if __name__ == '__main__':
