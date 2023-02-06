52224b1c40c48d69baee3d8aa96a9c2d8fc887ee
MadPapa <happyekkov@gmail.com>
2023-02-06 17:17:10 +0100

    catch data from API and display on ReactApp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,35 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [articles, setArticles] = useState([])
+  useEffect(() => {
+    fetch('http://127.0.0.1:5000/get', {
+      'method': 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(resp => resp.json())
+    .then(resp => setArticles(resp))
+    .catch(error => console.log(error))
+    
+  },[])
+
   return (
     <div className="App">
       <h1>Flask and React App</h1>
+      
+      {articles.map(article => {
+        return(
+          <div key = { article.id }>
+              <h2>{ article.title }</h2>
+              <p>{ article.body }</p>
+              <p>{ article.date }</p>
+          </div>
+        )
+      })}
     </div>
   );
 }
