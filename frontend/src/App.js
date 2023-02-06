f9de6a928585e3b14974f9bf49f8854a552c9e92
MadPapa <happyekkov@gmail.com>
2023-02-06 17:43:53 +0100

    import component ArticleList

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import { useState, useEffect } from 'react';
+import ArticleList from './components/ArticleList';
 
 function App() {
 
   const [articles, setArticles] = useState([])
+
   useEffect(() => {
     fetch('http://127.0.0.1:5000/get', {
       'method': 'GET',
@@ -11,25 +13,18 @@ function App() {
         'Content-Type': 'application/json'
       }
     })
-    .then(resp => resp.json())
-    .then(resp => setArticles(resp))
-    .catch(error => console.log(error))
-    
-  },[])
+      .then(resp => resp.json())
+      .then(resp => setArticles(resp))
+      .catch(error => console.log(error))
+
+  }, [])
 
   return (
     <div className="App">
       <h1>Flask and React App</h1>
-      
-      {articles.map(article => {
-        return(
-          <div key = { article.id }>
-              <h2>{ article.title }</h2>
-              <p>{ article.body }</p>
-              <p>{ article.date }</p>
-          </div>
-        )
-      })}
+
+      <ArticleList articles={articles} />
+
     </div>
   );
 }
